ffa9d0bc8c1e0fdc3b1f01b29033bf822be6a738
pcu.compus <njw3458@gmail.com>
2023-09-25 16:25:04 +0900

    방향키로 비행선 움직이기/ 방향키 아스키코드로 변환

--- 비행기 만들기/ship.js
+++ 비행기 만들기/ship.js
@@ -3,51 +3,70 @@ var sx, sy;
 var stype = 0;
 var sp = 0;
 
-function clearCanvas(){
+// 방향키 관련 변수 추가
+var leftPressed = false;
+var rightPressed = false;
+var upPressed = false;
+var downPressed = false;
+
+function clearCanvas() {
     ctx.clearRect(0, 0, vcanvas.width, vcanvas.height);
 }
 
-function drawShip(){  
+function drawShip() {
     ctx.beginPath();
     ctx.fillStyle = "red";
-    ctx.moveTo(sx-15, sy);
-    ctx.lineTo(sx, sy-15);
-    ctx.lineTo(sx+60, sy);
-    ctx.lineTo(sx, sy+15);
+    ctx.moveTo(sx - 15, sy);
+    ctx.lineTo(sx, sy - 15);
+    ctx.lineTo(sx + 60, sy);
+    ctx.lineTo(sx, sy + 15);
     ctx.closePath();
     ctx.fill();
 
-    if(stype === 1){
-        ctx.fillRect(sx+42, sy-5, 10, 10);
+    if (stype === 1) {
+        ctx.fillRect(sx + 42, sy - 5, 10, 10);
     }
-    if(stype > 1){
-        ctx.fillRect(sx+10, sy-15, 10, 30);
-    }    
-    if(stype === 3){
-        ctx.fillRect(sx, sy-19, 10, 38);
+    if (stype > 1) {
+        ctx.fillRect(sx + 10, sy - 15, 10, 30);
+    }
+    if (stype === 3) {
+        ctx.fillRect(sx, sy - 19, 10, 38);
     }
 }
 
-
-function drawBullet(){
-    // if(sp === 1){ // 스페이스바가 눌렸을 때만 총알을 그립니다.
-    if(sp){ 
+function drawBullet() {
+    if (sp === 1) {
         ctx.fillStyle = "green";
-        if(stype != 1){
-            ctx.fillRect(sx+60, sy-2, 5, 4);
+        if (stype !== 1) {
+            ctx.fillRect(sx + 60, sy - 2, 5, 4);
         }
-        if(stype === 1){
-            ctx.fillRect(sx+52, sy-5, 5, 4);
-            ctx.fillRect(sx+52, sy+1, 5, 4);        
+        if (stype === 1) {
+            ctx.fillRect(sx + 52, sy - 5, 5, 4);
+            ctx.fillRect(sx + 52, sy + 1, 5, 4);
         }
-        if(stype > 1){
-            ctx.fillRect(sx+20, sy-15, 5, 4);
-            ctx.fillRect(sx+20, sy+11, 5, 4);        
+        if (stype > 1) {
+            ctx.fillRect(sx + 20, sy - 15, 5, 4);
+            ctx.fillRect(sx + 20, sy + 11, 5, 4);
         }
-        if(stype === 3){
-            ctx.fillRect(sx+10, sy-19, 5, 4);
-            ctx.fillRect(sx+10, sy+15, 5, 4);        
+        if (stype === 3) {
+            ctx.fillRect(sx + 10, sy - 19, 5, 4);
+            ctx.fillRect(sx + 10, sy + 15, 5, 4);
         }
+    }
+}
+
+function moveShip() {
+    if (leftPressed && sx > 0) {
+        sx -= 2;
+    }
+    if (rightPressed && sx < vcanvas.width - 60) {
+        sx += 2;
+    }
+    if (upPressed && sy > 0) {
+        sy -= 2;
+    }
+    if (downPressed && sy < vcanvas.height - 15) {
+        sy += 2;
     }
 }
 
@@ -55,9 +74,10 @@ function gameLoop() {
     clearCanvas();
     drawShip();
     drawBullet();
-}
+    moveShip();
+    requestAnimationFrame(gameLoop); 
 
-function init(){
+function init() {
     vcanvas = document.getElementById("myCanvas");
     ctx = vcanvas.getContext("2d");
 
@@ -65,26 +85,69 @@ function init(){
     sy = 200;
     sp = 0;
 
-    setInterval(gameLoop, 33);
+    document.addEventListener("keydown", keyDownHandler);
+    document.addEventListener("keyup", keyUpHandler);
+    requestAnimationFrame(gameLoop); 
 }
 
-// key Control
+// Key Control
+function setKey(event) {
+    if (event.keyCode === 48) {
+        stype = 0;
+    }
+    if (event.keyCode === 49) {
+        stype = 1;
+    }
+    if (event.keyCode === 50) {
+        stype = 2;
+    }
+    if (event.keyCode === 51) {
+        stype = 3;
+    }
 
-function setKey(event){
-    if(event.keyCode === 48){ stype = 0; }
-    if(event.keyCode === 49){ stype = 1; }
-    if(event.keyCode === 50){ stype = 2; }
-    if(event.keyCode === 51){ stype = 3; }
-
-    if(event.keyCode === 32){ sp = 1; }
-
+    if (event.keyCode === 32) {
+        sp = 1;
+    }
 }
 
 function stopKey(event) {
-    if (event.keyCode === 32) { // 스페이스바를 떼면
-        sp = 0; // sp 변수를 0으로 설정하여 총알 발사를 중지합니다.
+    if (event.keyCode === 32) {
+        sp = 0;
+    }
+}
+
+// 방향키 이벤트 핸들러 함수 추가
+function keyDownHandler(event) {
+    if (event.keyCode === 37) { // 왼쪽방향키
+        leftPressed = true;
+    }
+    if (event.keyCode === 39) { // 오른쪽방향키
+        rightPressed = true;
+    }
+    if (event.keyCode === 38) { // 위쪽방향키
+        upPressed = true;
+    }
+    if (event.keyCode === 40) { // 아래방향키
+        downPressed = true;
+    }
+}
+
+function keyUpHandler(event) {
+    if (event.keyCode === 37) { // 왼쪽방향키
+        leftPressed = false;
+    }
+    if (event.keyCode === 39) { // 오른쪽방향키
+        rightPressed = false;
+    }
+    if (event.keyCode === 38) { // 위쪽방향키
+        upPressed = false;
+    }
+    if (event.keyCode === 40) { // 아래방향키
+        downPressed = false;
     }
 }
 
 document.onkeydown = setKey;
-document.onkeyup = stopKey; // 스페이스바 떼는 이벤트 핸들러를 추가합니다.
+document.onkeyup = stopKey;
+
+init(); 
