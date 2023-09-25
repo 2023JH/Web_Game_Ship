835985f7490f5853b7508a03c02dc589592fa8c0
pcu.compus <njw3458@gmail.com>
2023-09-25 15:38:42 +0900

    비행기 총알만들기 / 스페이스바 눌렀을때만 반응

--- 비행기 만들기/ship.js
+++ 비행기 만들기/ship.js
@@ -1,6 +1,7 @@
 var vcanvas, ctx;
 var sx, sy;
-var stype = 0; // You need to declare the 'stype' variable.
+var stype = 0;
+var sp = 0;
 
 function clearCanvas(){
     ctx.clearRect(0, 0, vcanvas.width, vcanvas.height);
@@ -27,9 +28,33 @@ function drawShip(){
     }
 }
 
+
+function drawBullet(){
+    // if(sp === 1){ // 스페이스바가 눌렸을 때만 총알을 그립니다.
+    if(sp){ 
+        ctx.fillStyle = "green";
+        if(stype != 1){
+            ctx.fillRect(sx+60, sy-2, 5, 4);
+        }
+        if(stype === 1){
+            ctx.fillRect(sx+52, sy-5, 5, 4);
+            ctx.fillRect(sx+52, sy+1, 5, 4);        
+        }
+        if(stype > 1){
+            ctx.fillRect(sx+20, sy-15, 5, 4);
+            ctx.fillRect(sx+20, sy+11, 5, 4);        
+        }
+        if(stype === 3){
+            ctx.fillRect(sx+10, sy-19, 5, 4);
+            ctx.fillRect(sx+10, sy+15, 5, 4);        
+        }
+    }
+}
+
 function gameLoop() {
     clearCanvas();
     drawShip();
+    drawBullet();
 }
 
 function init(){
@@ -38,6 +63,7 @@ function init(){
 
     sx = 200;
     sy = 200;
+    sp = 0;
 
     setInterval(gameLoop, 33);
 }
@@ -49,6 +75,16 @@ function setKey(event){
     if(event.keyCode === 49){ stype = 1; }
     if(event.keyCode === 50){ stype = 2; }
     if(event.keyCode === 51){ stype = 3; }
+
+    if(event.keyCode === 32){ sp = 1; }
+
+}
+
+function stopKey(event) {
+    if (event.keyCode === 32) { // 스페이스바를 떼면
+        sp = 0; // sp 변수를 0으로 설정하여 총알 발사를 중지합니다.
+    }
 }
 
 document.onkeydown = setKey;
+document.onkeyup = stopKey; // 스페이스바 떼는 이벤트 핸들러를 추가합니다.
