aa247258a5bb9b2bd2cc1b2b4a66d0bced5b4240
pcu.compus <njw3458@gmail.com>
2023-11-13 14:37:37 +0900

    사각형과 원 충돌테스트

--- base.js
+++ base.js
@@ -1,11 +1,22 @@
 'use strict'
 var vcanvas, ctx;
 var r_left, r_up, r_right, r_down;
-var ct = {x: 500, y: 100, r: 50, v: 1};
-var rt = {x: 200, y: 100, w: 200, h: 150};
+var ct = {x: 510, y: 100, r: 50, v: 1};
+var rt = {x: 200, y: 150, w: 200, h: 150};
 
 function clearCanvas(){
     ctx.clearRect(0, 0, vcanvas.width, vcanvas.height);
+}
+
+function checkHit(rt, ct){
+    var vResult = false;
+}
+
+function updateCT(){
+    if(r_left) {ct.x -= ct.v}
+    if(r_up) {ct.y -= ct.v}
+    if(r_right) {ct.x += ct.v}
+    if(r_down) {ct.y += ct.v}
 }
 
 function drawCT(){
@@ -17,7 +28,7 @@ function drawCT(){
     ctx.arc(ct.x, ct.y, 2, 0, Math.PI * 2);
     ctx.stroke();
     //text
-    ctx.fillText("(ct.x, ct.y)", ct.x - 30, ct.y + 8);
+    ctx.fillText("(ct.x, ct.y)", ct.x - 30, ct.y + 10);
 }
 
 function drawRT(){
@@ -28,16 +39,36 @@ function drawRT(){
     ctx.fillText("(x1, y1)", rt.x + rt.w -20, rt.y + rt.h + 15);
 }
 
+function drawExtend(){
+   // ctx.strokeRect(rt.x-ct.r, rt.y-ct.r, rt.w+ct.r*2 , rt.h+ct.r*2);
+    ctx.strokeStyle = "red";
+    ctx.setLineDash([1]);
+
+    ctx.beginPath();
+    ctx.moveTo(rt.x - ct.r, rt.y - ct.r);
+    ctx.lineTo(rt.x + rt.w + ct.r, rt.y - ct.r);
+    ctx.lineTo(rt.x + rt.w + ct.r, rt.y + rt.h + ct.r);
+    ctx.lineTo(rt.x - ct.r, rt.y + rt.h + ct.r);
+    ctx.closePath();
+    
+    ctx.stroke();
+    ctx.setLineDash([0]);
+    ctx.strokeStyle = "black";
+}
+
 function gameLoop(){
     clearCanvas();
+    updateCT();
     drawCT();
     drawRT();
+    drawExtend();
 }
 
 function init() {
     vcanvas = document.getElementById("myCanvas");
     ctx = vcanvas.getContext("2d");
 
+    ctx.font = "15px Arial";
     setInterval(gameLoop, 33);
 }
 // Key Control
